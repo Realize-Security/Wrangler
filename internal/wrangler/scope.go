e8b59361396cac5508bdc4a01033b48ec9cf2998
Richard Curteis <richard@realizesec.com>
2025-04-10 11:06:37 +0100

    optimise scope loading

--- internal/wrangler/scope.go
+++ internal/wrangler/scope.go
@@ -2,43 +2,76 @@ package wrangler
 
 import (
 	"Wrangler/internal/files"
+	"Wrangler/pkg/helpers"
 	"Wrangler/pkg/validators"
 	"fmt"
 	"os"
 	"strings"
 )
 
+// FlattenScopes flattens IP addresses, FQDNs and CIDRs into individual IP addresses
 func (wr *wranglerRepository) FlattenScopes(paths string) ([]string, error) {
-	scopes := strings.Split(paths, ",")
+	rawScopes := strings.Split(paths, ",")
+	scopes := make([]string, 0, len(rawScopes))
+	for _, s := range rawScopes {
+		s = strings.TrimSpace(s)
+		if s != "" {
+			scopes = append(scopes, s)
+		}
+	}
+	if len(scopes) == 0 {
+		return nil, fmt.Errorf("no scopes specified")
+	}
+
 	for _, scope := range scopes {
-		scope = strings.TrimSpace(scope)
 		if _, err := os.Stat(scope); os.IsNotExist(err) {
 			return nil, fmt.Errorf("file %s does not exist", scope)
 		}
 	}
-	var allIps []string
+
+	uniqueIPs := make(map[string]struct{})
 
 	for _, scope := range scopes {
-		ips, err := files.FileLinesToSlice(scope)
+		lines, err := files.FileLinesToSlice(scope)
 		if err != nil {
-			return nil, fmt.Errorf("unable to parse: %s. error: %s", scope, err.Error())
+			return nil, fmt.Errorf("unable to parse file %s: %v", scope, err)
 		}
-		allIps = append(allIps, ips...)
+		for _, line := range lines {
+			expanded := expandCIDR(line)
+
+			if len(expanded) == 0 {
+				uniqueIPs[line] = struct{}{}
+				continue
+			}
+
+			for _, ip := range expanded {
+				uniqueIPs[ip] = struct{}{}
+			}
+		}
 	}
 
-	ipLen := len(allIps)
-	final := make([]string, 0, ipLen)
-	uniqueIps := make(map[string]bool, ipLen)
+	if len(uniqueIPs) == 0 {
+		return nil, fmt.Errorf("empty target set")
+	}
 
-	for _, ip := range allIps {
-		if !uniqueIps[ip] {
-			uniqueIps[ip] = true
-			final = append(final, ip)
-		}
+	final := make([]string, 0, len(uniqueIPs))
+	for ip := range uniqueIPs {
+		final = append(final, ip)
 	}
 
 	if err := validators.ValidateScope(final); err != nil {
 		return nil, err
 	}
 	return final, nil
+}
+
+func expandCIDR(cidr string) []string {
+	finalIps := make([]string, 0)
+	res := helpers.ParseIPV4CIDR(cidr)
+	if res != nil {
+		finalIps = append(finalIps, res...)
+	} else {
+		finalIps = append(finalIps, cidr)
+	}
+	return finalIps
 }
