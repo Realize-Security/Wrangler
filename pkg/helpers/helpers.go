740d8b3b23bb040fa4dbe563244639de8b200737
Richard Curteis <richard@realizesec.com>
2025-05-14 14:08:10 +0100

    removed ReadNFromSlice

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -7,15 +7,3 @@ import (
 func SpacesToUnderscores(description string) string {
 	return strings.Replace(strings.ToLower(description), " ", "_", -1)
 }
-
-// ReadNFromSlice reads up to n values from a slice and returns them as a new slice.
-// If the input slice has fewer than n elements, all available elements are returned.
-func ReadNFromSlice[T any](slice []T, n int) []T {
-	if n <= 0 {
-		return []T{}
-	}
-	if len(slice) <= n {
-		return slice
-	}
-	return slice[:n]
-}
